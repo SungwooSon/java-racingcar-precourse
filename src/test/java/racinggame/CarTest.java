beeab6391d88437cd302bf313a5e6ec1475e96a9
sungwoo <ssw890418@gmail.com>
2021-10-11 14:54:35 +0900

    feat:전진, 멈춤 기능 테스트 구현
    숫자값이 전달되었을때, 자동차가 전진 또는 멈출지 결정하는 기능을 테스트한다

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,2 +1,24 @@
-package racinggame;public class CarTest {
+package racinggame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    void 전진() {
+        Car car = new Car("pobi");
+        car.move(4);
+        int position = car.getPosition();
+        assertThat(position).isGreaterThan(0);
+    }
+
+    @Test
+    void 스탑() {
+        Car car = new Car("honux");
+        car.move(3);
+        int position = car.getPosition();
+        assertThat(position).isEqualTo(0);
+    }
+
 }
