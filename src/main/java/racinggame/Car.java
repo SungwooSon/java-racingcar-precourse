69cf7a640b066dd5bced824e079b956bcb65c2e8
sungwoo <ssw890418@gmail.com>
2021-10-11 14:52:44 +0900

    feat:전진,멈춤 기능 구현
    숫자값이 전달되었을때, 전진할지 멈출지 결정한다

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -1,6 +1,27 @@
 package racinggame;
 
 public class Car {
-    public Car(String pobi) {
+    private final int CRITERION_MOVE_OR_STOP = 4;
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int number) {
+        if(isMove(number)) {
+            position++;
+        }
+    }
+
+    private boolean isMove(int number) {
+        return number >= CRITERION_MOVE_OR_STOP;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
