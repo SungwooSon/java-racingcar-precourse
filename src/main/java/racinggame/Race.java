eee66d215c32cdad6a9da2f7d92d82f0ecd3a853
sungwoo <ssw890418@gmail.com>
2021-10-12 16:55:34 +0900

    feat:우승자 리스트를 만드는 기능 구현
    가장 멀리간 자동차들을 List에 담아서 반환하는 메소드 구현

--- src/main/java/racinggame/Race.java
+++ src/main/java/racinggame/Race.java
@@ -1,2 +1,38 @@
-package racinggame;public class Race {
+package racinggame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private List<Car> carList;
+    private List<Car> winnerList;
+    private Car farthestMovingCar;
+
+    public Race(List<Car> list) {
+        this.carList = list;
+        winnerList = new ArrayList<>();
+    }
+
+    public List<Car> getWinnerList() {
+        farthestMovingCar = carList.get(0);
+        for(Car car : carList) {
+            clearWinnerListIfChangeFarthestMovingCar(car);
+            addListIfFarthestMovingCar(car);
+        }
+        return winnerList;
+    }
+
+    private void clearWinnerListIfChangeFarthestMovingCar(Car car) {
+        if(car.compareTo(farthestMovingCar) > 0) {
+            winnerList.clear();
+        }
+    }
+
+    private void addListIfFarthestMovingCar(Car car) {
+        if(car.compareTo(farthestMovingCar) >= 0) {
+            winnerList.add(car);
+            farthestMovingCar = car;
+        }
+    }
 }
--- src/test/java/racinggame/RaceTest.java
+++ src/test/java/racinggame/RaceTest.java
@@ -1,2 +1,32 @@
-package racinggame;public class RaceTest {
+package racinggame;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RaceTest {
+
+    @Test
+    void 가장_멀리간_자동차_리스트_만들기_테스트() {
+        Car pobiCar = new Car("pobi");
+        Car honuxCar = new Car("honux");
+        Car crongCar = new Car("crong");
+
+        pobiCar.move(9);
+        pobiCar.move(9);
+        pobiCar.move(3);
+        honuxCar.move(9);
+        honuxCar.move(1);
+        honuxCar.move(2);
+        crongCar.move(9);
+        crongCar.move(9);
+        crongCar.move(9);
+
+        Race race = new Race(Arrays.asList(pobiCar, honuxCar, crongCar));
+        assertThat(race.getWinnerList()).containsExactly(crongCar);
+        assertThat(race.getWinnerList()).doesNotContain(honuxCar);
+    }
+
 }
