bc560d47bbf9c5e107f689bca48cc112a6f1966c
sungwoo <ssw890418@gmail.com>
2021-10-13 17:23:35 +0900

    refactor:일급콜렉션을 사용하여 테스트하도록 수정

--- src/test/java/racinggame/RaceTest.java
+++ src/test/java/racinggame/RaceTest.java
@@ -2,7 +2,6 @@ package racinggame;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,10 +10,10 @@ public class RaceTest {
 
     @Test
     void 가장_멀리간_자동차_리스트_만들기_테스트() {
-        //Race race = new Race(Arrays.asList(pobiCar, honuxCar, crongCar));
         Race race = new Race();
-        race.makeCarList("pobi,honux,crong");
-        List<Car> carList = race.getCarList();
+        //race.makeCarList("pobi,honux,crong");
+        CarGroup carGroup = new CarGroup("pobi,honux,crong");
+        List<Car> carList = carGroup.getCarList();
 
         Car pobiCar = carList.get(0);
         Car honuxCar = carList.get(1);
@@ -30,15 +29,16 @@ public class RaceTest {
         crongCar.move(9);
         crongCar.move(9);
 
-        assertThat(race.getWinnerList()).containsExactly(crongCar);
-        assertThat(race.getWinnerList()).doesNotContain(honuxCar);
+        WinnerCarGroup winnerCarGroup = new WinnerCarGroup();
+        List<Car> winnerList = winnerCarGroup.makeWinnerList(carList);
+        assertThat(winnerList).containsExactly(crongCar);
+        assertThat(winnerList).doesNotContain(honuxCar,pobiCar);
     }
 
     @Test
     void 자동차_리스트_생성_테스트() {
-        Race race = new Race();
-        race.makeCarList("pobi,crong,honux");
-        List<Car> carList = race.getCarList();
+        CarGroup carGroup = new CarGroup("pobi,crong,honux");
+        List<Car> carList = carGroup.getCarList();
         assertThat(carList).containsExactly(new Car("pobi"), new Car("crong"), new Car("honux"));
     }
 }
