ba1e386ce032315a936afd9b5d9d72dcc7685e03
sungwoo <ssw890418@gmail.com>
2021-10-13 16:30:20 +0900

    feat:우승자리스트를 일급콜렉션으로 생성하도록 구현
    우승자 리스트와 관련된 로직을 일급콜렉션을 사용하도록 기능 구현

--- src/main/java/racinggame/WinnerCarGroup.java
+++ src/main/java/racinggame/WinnerCarGroup.java
@@ -1,2 +1,44 @@
-package racinggame;public class WinnerCarGroup {
+package racinggame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerCarGroup {
+
+    private List<Car> winnerList;
+
+    public WinnerCarGroup() {
+        winnerList = new ArrayList<>();
+    }
+
+    public List<Car> makeWinnerList(List<Car> carList) {
+        Car farthestMovingCar = carList.get(0);
+        for(Car car : carList) {
+            farthestMovingCar = clearWinnerListIfChangeFarthestMovingCar(car, farthestMovingCar);
+            addListIfFarthestMovingCar(car, farthestMovingCar);
+        }
+        return winnerList;
+    }
+
+    private Car clearWinnerListIfChangeFarthestMovingCar(Car car, Car farthestMovingCar) {
+        if(isChangedFarthestMovingCar(car, farthestMovingCar)) {
+            winnerList.clear();
+            return car;
+        }
+        return farthestMovingCar;
+    }
+
+    private boolean isChangedFarthestMovingCar(Car car, Car farthestMovingCar) {
+        return car.compareTo(farthestMovingCar) > 0;
+    }
+
+    private void addListIfFarthestMovingCar(Car car, Car farthestMovingCar) {
+        if(isAddCar(car, farthestMovingCar)) {
+            winnerList.add(car);
+        }
+    }
+
+    private boolean isAddCar(Car car, Car farthestMovingCar) {
+        return car.compareTo(farthestMovingCar) >= 0;
+    }
 }
