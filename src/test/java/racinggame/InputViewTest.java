f43e3df76e208cda2aecaa4df8c54bc3911694ff
sungwoo <ssw890418@gmail.com>
2021-10-13 10:15:19 +0900

    feat:자동차 이름이 5자리 이하인지 테스트하는 기능 구현
    문자열을 ,로 분리했을때 각각의 문자열들이 5자리 이하인지 검증한다

--- src/test/java/racinggame/InputViewTest.java
+++ src/test/java/racinggame/InputViewTest.java
@@ -1,2 +1,21 @@
-package racinggame;public class InputViewTest {
+package racinggame;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class InputViewTest {
+
+    @Test
+    void 자동차_이름_5자리_이하_테스트() {
+        String carNames = "pobi,honux,crongcrong";
+
+        assertThatExceptionOfType(IllegalStateException.class).isThrownBy(
+                () -> InputView.validateCarNames(carNames)
+        );
+    }
+
 }
