c7b91c21738ae1d707ba7d4ec08d6776d4051cb2
sungwoo <ssw890418@gmail.com>
2021-10-13 17:10:32 +0900

    feat:위치값의 원시값을 포장하는 클래스 구현
    위치값은 최초 생성시 0임을 보장하는 기능 구현

--- src/main/java/racinggame/Position.java
+++ src/main/java/racinggame/Position.java
@@ -1,2 +1,26 @@
-package racinggame;public class Position {
+package racinggame;
+
+public class Position {
+
+    private final int INITIAL_POSITION = 0;
+
+    private int position;
+
+    public Position(int number) {
+        validateInitialPosition(number);
+        this.position = number;
+    }
+
+    private void validateInitialPosition(int number) {
+        if(number != INITIAL_POSITION)
+            throw new IllegalStateException("포지션의 최초 위치는 항상 0이어야 합니다.");
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
 }
