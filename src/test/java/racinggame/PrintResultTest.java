e06570bac224b97b18fe78669d92aecfdff0197b
sungwoo <ssw890418@gmail.com>
2021-10-13 10:24:43 +0900

    feat:자동차가 움직인 만큼 대시 기호를 찍는 테스트 구현
    자동차가 움직인 만큼 대시 기호를 찍는다

--- src/test/java/racinggame/PrintResultTest.java
+++ src/test/java/racinggame/PrintResultTest.java
@@ -1,2 +1,20 @@
-package racinggame;public class PrintResult {
+package racinggame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrintResultTest {
+
+    @Test
+    void 위치_만큼_대시_기호_찍기_테스트() {
+        Car car = new Car("pobi");
+        car.move(9);
+        car.move(1);
+        car.move(7);
+
+        assertThat(car.getPosition()).isEqualTo(2);
+        assertThat(PrintResult.printCarPoition(car)).contains("pobi");
+        assertThat(PrintResult.printCarPoition(car)).contains("--");
+    }
 }
