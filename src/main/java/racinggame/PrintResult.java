24eedbc8ed2d4c4746c96abe3deac9372b68add8
sungwoo <ssw890418@gmail.com>
2021-10-13 10:26:11 +0900

    feat:자동차가 움직인 만큼 대시 기호를 출력하는 기능 구현

--- src/main/java/racinggame/PrintResult.java
+++ src/main/java/racinggame/PrintResult.java
@@ -1,2 +1,31 @@
-package racinggame;public class PrintResult {
+package racinggame;
+
+public class PrintResult {
+
+    private final static String DELIMETER = "-";
+    static StringBuilder sb;
+
+    public static String printCarPoition(Car car) {
+        sb = new StringBuilder(); // single ton으로 리팩터링.
+
+        appendCarName(car);
+        appendDelimeter(car);
+        printResult();
+
+        return sb.toString();
+    }
+
+    private static void appendCarName(Car car) {
+        sb.append(car.getName() + ":");
+    }
+
+    private static void appendDelimeter(Car car) {
+        for(int i=0 ; i<car.getPosition(); i++) {
+            sb.append(DELIMETER);
+        }
+    }
+
+    private static void printResult() {
+        System.out.println(sb.toString());
+    }
 }
